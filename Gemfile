426b2e5a5105a70439c14650dc5047af0579bb93
novohispano <novohispano@gmail.com>
2014-04-30 14:15:19 -0600

    Implement validations on person

--- Gemfile
+++ Gemfile
@@ -19,4 +19,5 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'better_errors'
   gem 'binding_of_caller'
+  gem 'simplecov'
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Person do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:person) do
+    Person.create(first_name: 'Jack', last_name: 'Smith')
+  end
+
+  it 'is valid' do
+    expect(person).to be_valid
+  end
+
+  it 'has a first name' do
+    expect(person).to be_valid
+    expect(person.first_name).to eq 'Jack'
+  end
+
+  it 'is invalid without a first name' do
+    person.first_name = nil
+    expect(person).not_to be_valid
+  end
+
+  it 'is invalid without a last name' do
+    person.last_name = nil
+    expect(person).not_to be_valid
+  end
 end
