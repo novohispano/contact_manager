9b8de3438a2871412364b0522f26b1cf46796e6f
novohispano <novohispano@gmail.com>
2014-05-01 13:43:52 -0600

    Add company model specs

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Company do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:company) { Company.create!(name: 'Lockheed') }
+
+  it 'is valid' do
+    expect(company).to be_valid
+  end
+
+  it 'has a name' do
+    expect(company.name).not_to be_nil
+    expect(company.name).to eq 'Lockheed'
+  end
+
+  it 'is invalid without a name' do
+    company.name = nil
+    expect(company).to be_invalid
+  end
 end
