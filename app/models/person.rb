8089464abc31dd6186e6b616f6e0e07ff236c24f
novohispano <novohispano@gmail.com>
2014-05-01 15:59:57 -0600

    Create Contact concern

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,5 @@
 class Person < ActiveRecord::Base
-  validates :first_name, :last_name, presence: true
+  include Contact
 
-  has_many :phone_numbers, as: :contact
-  has_many :email_addresses, as: :contact
+  validates :first_name, :last_name, presence: true
 end
